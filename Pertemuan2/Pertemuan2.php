bfdb6a3bab95b8d5098aae930acf0b51229b819f
Sukron <sukron.213040095@mail.unpas.ac.id>
2022-05-27 03:58:39 +0700

    Pertemuan 2

--- Pertemuan2/Pertemuan2.php
+++ Pertemuan2/Pertemuan2.php
@@ -2,4 +2,10 @@
 
 // Pertemuan 2
 
-echo "Sukron";
+$nama = "Sukron";
+$nim = "213040095";
+
+$mahasiswa = [$nama, $nim];
+print_r($mahasiswa);
+
+echo "Halo nama saya " . $nama . " nim saya " . $nim;
