d4ebed3114a0d128f76eb61f0f6534f5909a758a
Sukron <sukron.213040095@mail.unpas.ac.id>
2022-06-15 21:18:13 +0700

    Update pertemuan 2

--- Pertemuan2/Pertemuan2.php
+++ Pertemuan2/Pertemuan2.php
@@ -8,4 +8,20 @@ $nim = "213040095";
 $mahasiswa = [$nama, $nim];
 print_r($mahasiswa);
 
-echo "Halo nama saya " . $nama . " nim saya " . $nim;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    Selamat datang, nama saya <?php echo $nama; ?>
+</body>
+
+</html>
